946d31504786cf78224a46fbc945d6c1964fbc19
gustavomoura628 <gustavomoura@discente.ufg.br>
2022-01-26 10:40:32 -0300

    update

--- git.lua
+++ git.lua
@@ -2,7 +2,7 @@ local gitdata = io.open('gitdata','r')
 while true do
     local line = gitdata:read()
     if line == nil then break end
-    local files,filee = string.find(line,'^[^ ]')
+    local files,filee = string.find(line,'^[^ ]*')
     local file = string.sub(line,files,filee)
     local address = string.sub(line,filee+2)
     print("File: "..file..", Address: "..address)
