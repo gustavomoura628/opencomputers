df026b5857f3e17f82621050409b368711944d68
gustavomoura628 <gustavomoura@discente.ufg.br>
2022-01-26 10:53:00 -0300

    update

--- git.lua
+++ git.lua
@@ -8,7 +8,7 @@ while true do
     local file = string.sub(line,files,filee)
     local address = string.sub(line,filee+2)
     print("File: "..file..", Address: "..address)
-    local fileHandle = io.open(file,"w+")
+    local fileHandle = io.open(file,"w")
     local webHandle = internet.request(address)
     local webText = ""
     for chunk in webHandle do webText = webText..chunk end
