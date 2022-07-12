d5d0c1ff911222f309793cf2cfa630d5a32e5247
gustavomoura628 <gustavomoura@discente.ufg.br>
2022-07-11 21:16:53 -0300

    gud

---
+++ git2/temp/metadata.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+function gettimestamp(){
+    file=$1
+    git log --format=%ci $file | tail -n 1 | perl -pe 's/^(.*) .*/$1/' | perl -pe 's/[- :+]//g'
+}
+function gentimestamp(){
+    [ -f ".gitmetadata" ] && rm .gitmetadata
+    lastedit=0
+    filetype="file"
+    for file in $(ls);do
+        if [ -f "$file" ];then
+            time=$(gettimestamp $file)
+            filetype="file"
+        fi
+        if [ -d "$file" ];then
+            cd $file
+            time=$(gentimestamp)
+            filetype="folder"
+            cd ..
+        fi
+        [ -z "$time" ] && time=0
+        if [ "$time" -ge "$lastedit" ];then
+            lastedit=$time
+        fi
+        echo $filetype $file $time >> .gitmetadata
+    done
+    echo $lastedit
+}
+
+gentimestamp
